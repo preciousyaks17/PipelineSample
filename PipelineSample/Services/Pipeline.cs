6274efcbb1496a9e7109187bcbc0a16841dacb65
ABDULLAHI OLADIMEJI <b-aoladimeji@microsoft.com>
2024-02-20 17:32:16 +0100

    Worked on Backend

---
+++ PipelineSample/Services/Pipeline.cs
@@ -0,0 +1,29 @@
+﻿namespace PipelineSample.Services
+{
+    public class Pipeline
+    {
+       
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        
+        public bool ValidateDeposit(decimal amount)
+        {
+            if (amount > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        
+        public bool ValidateWithdraw(decimal amount, decimal balance)
+        {
+            if (amount > 0 && amount <= balance)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
